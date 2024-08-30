bbb0723c8612bdaa91ae3652fcb49c240afa7e2c
johnsharrar <js1025519@wcupa.edu>
2024-08-30 11:23:42 -0400

    Completed Week 1 assignments

--- ex2/src/main/java/Bulbasaur.java
+++ ex2/src/main/java/Bulbasaur.java
@@ -5,6 +5,8 @@ public class Bulbasaur {
     // Constructor
     public Bulbasaur() {
         // TODO: Initialize id and level with appropriate values
+        this.id = 1;
+        this.level = 1;
     }
 
     // setLevel method
@@ -13,25 +15,38 @@ public class Bulbasaur {
         // Update the level
         // If the new level is 16 or higher but less than 32, evolve to Ivysaur
         // If the new level is 32 or higher, evolve to Venusaur
+        this.level = lv;
+        if (this.level >= 16 && this.level < 32) {
+            this.id = 2;
+        } else if (this.level >= 32) {
+            this.id = 3;
+        }
     }
 
     // getLevel method
     public int getLevel() {
         // TODO: Implement this method
-        return 0; // Placeholder return value
+        return this.level; // Placeholder return value
     }
 
     // getName method
     public String getName() {
         // TODO: Implement this method
         // Return the name based on the current id
+        if (this.id == 1) {
+            return "Bulbasaur";
+        } else if (this.id == 2) {
+            return "Ivysaur";
+        } else if (this.id == 3) {
+            return "Venusaur";
+        }
         return ""; // Placeholder return value
     }
 
     // getID method
     public int getID() {
         // TODO: Implement this method
-        return 0; // Placeholder return value
+        return this.id; // Placeholder return value
     }
 
     // toString method
@@ -39,7 +54,7 @@ public class Bulbasaur {
     public String toString() {
         // TODO: Implement this method
         // Return a string representation of the Bulbasaur object
-        return ""; // Placeholder return value
+        return "Your Pokemon's name is: " + this.getName() + " and its level is " + this.getLevel(); // Placeholder return value
     }
 
     // equals method
@@ -47,6 +62,10 @@ public class Bulbasaur {
     public boolean equals(Object obj) {
         // TODO: Implement this method
         // Compare this Bulbasaur object with another object
+        if (obj instanceof Bulbasaur) {
+            Bulbasaur other = (Bulbasaur) obj;
+            return this.id == other.id && this.level == other.level;
+        }
         return false; // Placeholder return value
     }
 
@@ -54,6 +73,9 @@ public class Bulbasaur {
     public Bulbasaur copy() {
         // TODO: Implement this method
         // Create and return a new Bulbasaur object with the same id and level
-        return null; // Placeholder return value
+        Bulbasaur copy = new Bulbasaur();
+        copy.id = this.id;
+        copy.level = this.level;
+        return copy; // Placeholder return value
     }
 }
